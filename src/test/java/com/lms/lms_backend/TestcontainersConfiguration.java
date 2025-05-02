5c39f8f19e92361c0d419bd28ad11b51e0f95369
shiva_513 <madiwalshiva25@gmail.com>
2025-05-02 15:06:29 +0530

    Added Spring Boot backend files

---
+++ src/test/java/com/lms/lms_backend/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.lms.lms_backend;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
